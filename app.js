75cf908dde03288e16b7f170c95d7002263c7ab6
muba123321/ <laarychambas@gmail.com>
2024-09-05 13:05:33 -0400

    created a userRoute to access user data that will be create in the models folder

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const userRoutes = require("./routes/userRoutes")
 
 const app = express();
 
 const port = 3000;
 
-app.listen(
-  port, () => console.log(`Server is running on port ${port}`));
+// adding parsing middleware
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// create routes
+app.use("/users", userRoutes);
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
