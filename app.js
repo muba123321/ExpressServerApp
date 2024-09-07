6213d7771801b48232869dd143f0af95933016e3
muba123321/ <laarychambas@gmail.com>
2024-09-07 18:24:59 -0400

    created a req logging middleware and app error-handling middleware

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const userRoutes = require("./routes/userRoutes")
+const userRoutes = require("./routes/userRoutes");
 const activityRoutes = require("./routes/activitiesRoutes");
+const morgan = require("morgan");
 
 const app = express();
 
@@ -12,13 +13,22 @@ const port = 3000;
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Logging Request data middleware
+app.use(morgan("dev"));
+
 // create routes
-app.get('/',
-    (req, res) => {
-        res.send('Welcome to the Home Page')
-        }
-)
+app.get("/", (req, res) => {
+  res.send("Welcome to the Home Page");
+});
 app.use("/users", userRoutes);
 app.use("/activities", activityRoutes);
+
+// App Error Handling middleware...
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).send({
+    message: err.message || "Internal Server Error",
+    error: err,
+  });
+});
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
