f601da2f5c19d012aa8f17e13fd80d7ca89e0eb1
muba123321/ <laarychambas@gmail.com>
2024-09-09 07:15:01 -0400

    completed views apage added stlyes

--- routes/homepageRoutes.js
+++ routes/homepageRoutes.js
@@ -2,9 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 let activities = require("../models/activityModel");
+let users = require("../models/userModel");
 
 router.get("/",(req, res) => {
   res.render("index", { activities });
 });
+
+router.get(
+  "/activities/create",(req, res) =>
+  res.render("createActivityPage", { users } )
+);
 
 module.exports = router;
